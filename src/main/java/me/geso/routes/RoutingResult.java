b3cbb2017c3c49ee3c80fd575956fa26112f16cb
Tokuhiro Matsuno <tokuhirom@gmail.com>
2014-08-24 13:59:27 +0900

    Added javadoc.

--- src/main/java/me/geso/routes/RoutingResult.java
+++ src/main/java/me/geso/routes/RoutingResult.java
@@ -1,26 +1,42 @@
 package me.geso.routes;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class RoutingResult<T> {
 	protected boolean methodAllowed;
 	T destination;
-	Map<String, String> captured = new HashMap<>();
+	Map<String, String> captured = new TreeMap<>();
 
+	/**
+	 * This method returns true if it denied by HTTP method mismatch.
+	 * False otherwise.
+	 * 
+	 * @return
+	 */
 	public boolean methodAllowed() {
 		return methodAllowed;
 	}
 
+	/**
+	 * Get destination object.
+	 * 
+	 * @return
+	 */
 	public T getDestination() {
 		return destination;
 	}
 
-	public void putCaptured(String name, String group) {
+	void putCaptured(String name, String group) {
 		captured.put(name, group);
 	}
 
-	public Map<String,String> getCaptured() {
+	/**
+	 * Get captured variables in Map.
+	 * 
+	 * @return
+	 */
+	public Map<String, String> getCaptured() {
 		return captured;
 	}
 }
--- src/main/java/me/geso/routes/WebRouter.java
+++ src/main/java/me/geso/routes/WebRouter.java
@@ -11,7 +11,14 @@ public class WebRouter<T> {
 		patterns = new ArrayList<HttpRoute<T>>();
 	}
 
-	public RoutingResult<T> match(String method, String path) {
+	/**
+	 * Get the path information destination.
+	 *
+	 * @param method HTTP method
+	 * @param path PATH_INFO
+	 * @return
+	 */
+	public RoutingResult<T> match(final String method, final String path) {
 		RoutingResult<T> retval = null;
 		for (HttpRoute<T> route : patterns) {
 			RoutingResult<T> result = route.match(method, path);
@@ -25,16 +32,38 @@ public class WebRouter<T> {
 		return retval;
 	}
 
-	public WebRouter<T> get(String path, T target) {
-		return addRoute(path, target, Arrays.asList("GET", "HEAD"));
+	/**
+	 * Register new route. It only matches GET/HEAD method.
+	 *
+	 * @param path
+	 * @param destination
+	 * @return
+	 */
+	public WebRouter<T> get(final String path, final T destination) {
+		return addRoute(path, destination, Arrays.asList("GET", "HEAD"));
 	}
 
-	public WebRouter<T> post(String path, T target) {
-		return addRoute(path, target, Arrays.asList("POST"));
+	/**
+	 * Register new route. It only matches POST method.
+	 *
+	 * @param path
+	 * @param destination
+	 * @return
+	 */
+	public WebRouter<T> post(final String path, final T destination) {
+		return addRoute(path, destination, Arrays.asList("POST"));
 	}
 
-	public WebRouter<T> addRoute(String path, T target, List<String> methods) {
-		patterns.add(new HttpRoute<T>(path, target, methods));
+	/**
+	 * Register new route.
+	 * 
+	 * @param path PATH_INFO
+	 * @param destination Destination object
+	 * @param methods HTTP methods it should be match.
+	 * @return
+	 */
+	public WebRouter<T> addRoute(final String path, final T destination, final List<String> methods) {
+		patterns.add(new HttpRoute<T>(path, destination, methods));
 		return this;
 	}
 }
