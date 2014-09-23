1008476c5dd1243b3ce060959ffa34c7a6e04b26
Tokuhiro Matsuno <tokuhirom@gmail.com>
2014-09-23 10:53:25 +0900

    Make WebRouter as Serializable. Because HttpServlet holds instance of this class.
    HttpServlet is a serializable class. It should'nt hold non-serializable
    instance.

--- src/main/java/me/geso/routes/WebRouter.java
+++ src/main/java/me/geso/routes/WebRouter.java
@@ -1,10 +1,13 @@
 package me.geso.routes;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class WebRouter<T> {
+public class WebRouter<T> implements Serializable {
+	private static final long serialVersionUID = 1L;
+
 	@Override
 	public String toString() {
 		return "WebRouter [patterns=" + getPatterns() + "]";
